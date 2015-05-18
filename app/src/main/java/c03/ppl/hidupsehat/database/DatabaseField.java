6d32da1706b7106d562546654cccff828823577b
irwanmul <mulyawan.irwan@gmail.com>
2015-05-18 17:16:38 +0700

    Update 18 Mei Irwan

--- app/src/main/java/c03/ppl/hidupsehat/database/DatabaseField.java
+++ app/src/main/java/c03/ppl/hidupsehat/database/DatabaseField.java
@@ -17,5 +17,26 @@ public class DatabaseField {
     public static final String USER_COLUMN_ID = "id";
     public static final String USER_COLUMN_KELAMIN = "kelamin";
 
-    //
+    //RESEP MAKANAN
+    public static final String RESEP_MAKANAN_TABLE = "resep_makanan";
+    public static final String RESEP_MAKANAN_ID_USER = "idUser";
+    public static final String RESEP_MAKANAN_ID = "id";
+    public static final String RESEP_MAKANAN_NAMA = "nama";
+    public static final String RESEP_MAKANAN_KALORI = "kalori";
+    public static final String RESEP_MAKANAN_CARA_MEMBUAT = "caraMembuat";
+    public static final String RESEP_MAKANAN_SARAN_PENYAJIAN = "saranPenyajian";
+
+
+    //BAHAN MAKANAN
+    public static  final  String BAHAN_MAKANAN_TABLE = "bahan_makanan";
+    public static  final  String BAHAN_MAKANAN_ID = "id";
+    public static  final  String BAHAN_MAKANAN_NAMA = "nama";
+    public static  final  String BAHAN_MAKANAN_KALORI = "kalori";
+
+
+    //RESEP DAN BAHAN
+    public static final String RESEP_DAN_BAHAN_TABLE = "resep_dan_bahan";
+    public static final String RESEP_DAN_BAHAN_ID_RESEP = "idResep";
+    public static final String RESEP_DAN_BAHAN_ID_BAHAN = "idBahan";
+    public static final String RESEP_DAN_BAHAN_KETERANGAN = "keterangan";
 }
