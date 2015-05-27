ab64d849f4b2d3d0e8f3b321e66c70441ec2deeb
irwanmul <mulyawan.irwan@gmail.com>
2015-05-27 11:01:50 +0700

    Daftar Achievement Olahraga

---
+++ app/src/main/java/c03/ppl/hidupsehat/Achievement/Olahraga.java
@@ -0,0 +1,18 @@
+package c03.ppl.hidupsehat.Achievement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import c03.ppl.hidupsehat.R;
+
+/**
+ * Created by Irwan on 27/05/2015.
+ */
+public class Olahraga extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.melihat_achievement_gagal);
+
+    }
+}
